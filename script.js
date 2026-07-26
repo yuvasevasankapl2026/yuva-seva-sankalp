1c03218b98191065698603f1610cf9d9ead08936
yuvasevasankapl2026 <yuvasevasankalp2026@gmail.com>
2026-07-26 17:42:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+// Smooth Scroll
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target){
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
 
+// Back To Top Button
+const topBtn = document.createElement("button");
+topBtn.innerHTML = "⬆";
+topBtn.id = "topBtn";
+document.body.appendChild(topBtn);
+
+topBtn.style.position = "fixed";
+topBtn.style.bottom = "20px";
+topBtn.style.right = "20px";
+topBtn.style.padding = "12px 15px";
+topBtn.style.border = "none";
+topBtn.style.borderRadius = "50%";
+topBtn.style.background = "#0b6b3a";
+topBtn.style.color = "#fff";
+topBtn.style.cursor = "pointer";
+topBtn.style.display = "none";
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
