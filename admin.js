17f95b21b7f18f94f74710b34395c1275af5a9e1
yuvasevasankapl2026 <yuvasevasankalp2026@gmail.com>
2026-08-02 23:17:52 +0530

    Implement admin login functionality

--- admin.js
+++ admin.js
@@ -1,1 +1,82 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    function () {
 
+        const adminLoginForm =
+        document.getElementById(
+            "adminLoginForm"
+        );
+
+
+        adminLoginForm.addEventListener(
+            "submit",
+            function (event) {
+
+                event.preventDefault();
+
+
+                const username =
+
+                document.getElementById(
+                    "adminUsername"
+                ).value.trim();
+
+
+                const password =
+
+                document.getElementById(
+                    "adminPassword"
+                ).value;
+
+
+                const message =
+
+                document.getElementById(
+                    "loginMessage"
+                );
+
+
+                if (
+                    username === "admin"
+                    &&
+                    password === "12345"
+                ) {
+
+                    message.style.color =
+                    "#247331";
+
+
+                    message.innerHTML =
+
+                    "लॉगिन सफल हो गया।";
+
+
+                    setTimeout(
+                        function () {
+
+                            window.location.href =
+                            "admin-dashboard.html";
+
+                        },
+                        1000
+                    );
+
+                }
+
+                else {
+
+                    message.style.color =
+                    "#d32f2f";
+
+
+                    message.innerHTML =
+
+                    "यूज़रनेम या पासवर्ड गलत है।";
+
+                }
+
+            }
+        );
+
+    }
+);
